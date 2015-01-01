600f97e2253e8e2acae1b431306d04f816c514bf
Tobi Hahn <rainshapes@gmail.com>
2015-01-01 12:45:35 -0500

    petit fire

---
+++ Assets/SwatFireAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwatFireAnimation : MonoBehaviour {
+	void Update () {
+		if(transform.parent.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("swat")){
+			renderer.enabled = true;
+			GetComponent<Animator>().Play("fire");
+		}
+		else renderer.enabled = false;
+	}
+}

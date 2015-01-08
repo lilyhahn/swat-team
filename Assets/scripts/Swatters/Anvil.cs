39571281d99d5657182ecc3f5650e6cf942dcdcf
Tobi Hahn <rainshapes@gmail.com>
2015-01-08 07:09:02 -0500

    work

--- Assets/scripts/Swatters/Anvil.cs
+++ Assets/scripts/Swatters/Anvil.cs
@@ -15,20 +15,20 @@ public class Anvil : Hand {
 	
 	protected override void Update(){
 		base.Update();
-		if(Input.GetButton("Fire1")){
+		if(Input.GetButton("Fire1") && !anim.GetCurrentAnimatorStateInfo(0).IsName("swat")){
 			charge_circle.speed = chargeRate;
 		}
 		else{
 			charge_circle.speed = 0;
 		}
-		if(charge_circle.GetCurrentAnimatorStateInfo(0).normalizedTime > 0){
+		if(charge_circle.GetCurrentAnimatorStateInfo(0).normalizedTime > 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("swat")){
 			charge_circle.GetComponent<SpriteRenderer>().enabled = true;
 		}
 		else{
 			charge_circle.GetComponent<SpriteRenderer>().enabled = false;
 		}
 		//GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, new Color(255, 255, 255, 1), Time.deltaTime * charge / 100);
-		if(Input.GetButtonUp("Fire1")){
+		if(Input.GetButton("Fire1")){
 			Swat ();
 		}
 		if(anim.GetCurrentAnimatorStateInfo(0).IsName("swat") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 2){

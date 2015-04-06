3933c42859bb4f044876f3445e7acf70f46f91d3
Tobi Hahn <thahn@scienceleadership.org>
2015-04-06 15:25:41 -0400

    blinking start prompt

---
+++ Assets/Blink.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blink : MonoBehaviour {
+	
+	public float blinkInterval;
+
+	void Start () {
+		InvokeRepeating ("ToggleRenderer", 0, blinkInterval);
+	}
+	
+	void ToggleRenderer(){
+		GetComponent<Renderer> ().enabled = !GetComponent<Renderer> ().enabled;
+	}
+}

154b3e27255b5032dd4e525ec648e424ef2008ce
Tobi Hahn <rainshapes@gmail.com>
2015-04-19 17:15:29 -0400

    adaptive control display

--- Assets/scripts/ControlDisplay.cs
+++ Assets/scripts/ControlDisplay.cs
@@ -5,17 +5,33 @@ public class ControlDisplay : MonoBehaviour {
 
 	public GameObject joystickControls;
 	public GameObject keyboardControls;
+    public GameObject comboControls;
 
 	// Use this for initialization
-	void Start () {
-		if(Input.GetJoystickNames().Length > 0){
-			joystickControls.SetActive(true);
-			keyboardControls.SetActive(false);
-		}
-		else{
-			keyboardControls.SetActive(true);
-			joystickControls.SetActive(false);
-		}
-	}
+    void Start() {
+        switch (Input.GetJoystickNames().Length) {
+            case 0:
+                keyboardControls.SetActive(true);
+                joystickControls.SetActive(false);
+                break;
+            case 1:
+                if (comboControls != null) {
+                    comboControls.SetActive(true);
+                    keyboardControls.SetActive(false);
+                    joystickControls.SetActive(false);
+                    break;
+                }
+                else goto case 2; //fuck c#
+            case 2:
+                joystickControls.SetActive(true);
+                keyboardControls.SetActive(false);
+                break;
+            default:
+                joystickControls.SetActive(true);
+                keyboardControls.SetActive(false);
+                comboControls.SetActive(false);
+                break;
+        }
+    }
 	
 }

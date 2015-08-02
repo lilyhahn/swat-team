4bf7e758ac72996f7ca33b6e3f6cf116312b4212
Tobi Hahn <rainshapes@gmail.com>
2015-08-02 09:37:29 -0400

    worm complete except for special

--- Assets/scripts/Bugs/Worm/Worm.cs
+++ Assets/scripts/Bugs/Worm/Worm.cs
@@ -7,45 +7,93 @@ public class Worm : Bug {
 
     Transform head;
 
-    public List<WormPart> bodyParts;
+    bool killing = false;
 
+    public List<WormPart> bodyParts;
     List<Vector3> partPositions = new List<Vector3>();
+    Vector2 partAnchor;
+    Vector2 partConnectedAnchor;
 
     override protected void Start() {
         base.Start();
         head = transform.Find("Head");
+        partAnchor = transform.Find("Body 1").GetComponent<HingeJoint2D>().anchor;
+        partConnectedAnchor = transform.Find("Body 1").GetComponent<HingeJoint2D>().connectedAnchor;
         foreach (Transform part in transform) {
             partPositions.Add(part.localPosition);
         }
     }
 
+    protected override void Update() {
+        base.Update();
+        Debug.Log(holdingBerry);
+    }
+
     public void Kill(WormPart part) {
-        if (!part.isHead) {
-            for (int i = bodyParts.IndexOf(part) - 1; i < bodyParts.Count; i++) {
-                bodyParts[i].GetComponent<HingeJoint2D>().enabled = false;
+        StartCoroutine(KillRoutine(part));
+    }
+
+    IEnumerator KillRoutine(WormPart part) {
+        if (!killing) {
+            killing = true;
+            if (!part.isHead && !holdingBerry) {
+                for (int i = bodyParts.IndexOf(part) - 1; i < bodyParts.Count; i++) {
+                    bodyParts[i].GetComponent<HingeJoint2D>().enabled = false;
+                }
             }
-        }
-        else {
-            bodyParts[0].Kill(WormPartStates.Dead);
-            for (int i = 0; i < bodyParts.Count; i++) {
-                bodyParts[i].Kill(WormPartStates.Dead);
-                bodyParts[i].GetComponent<HingeJoint2D>().enabled = false;
+            else {
+                if (holdingBerry) {
+                    moveSpeed = origMoveSpeed;
+                    maxSpeed = origMaxSpeed;
+                    berry.GetComponent<SpriteRenderer>().sprite = squishedBerries[Random.Range(0, squishedBerries.Length - 1)];
+                    berry.transform.parent = null;
+                    holdingBerry = false;
+                    GetComponent<AudioSource>().clip = berrySquishSound;
+                    GetComponent<AudioSource>().Play();
+                    int berryCount = 0;
+                    foreach (GameObject b in GameObject.FindGameObjectsWithTag("berry")) {
+                        if (b.transform.parent != null && b.transform.parent.tag == "berry tree") {
+                            berryCount++;
+                        }
+                    }
+                    if (berryCount + gameManager.bugScore < 3) {
+                        gameManager.EndGame(WinnerType.Human);
+                    }
+                }
+                else {
+                    bodyParts[0].Kill(WormPartStates.Dead);
+                    for (int i = 0; i < bodyParts.Count; i++) {
+                        bodyParts[i].Kill(WormPartStates.Dead);
+                        bodyParts[i].GetComponent<HingeJoint2D>().enabled = false;
+                    }
+                    if (gameManager.inGame) {
+                        gameManager.EndGame(WinnerType.Human);
+                    }
+                }
             }
-            if (gameManager.inGame) {
-                gameManager.EndGame(WinnerType.Human);
-            }
+            yield return new WaitForSeconds(1.5f);
+            killing = false;
         }
     }
 
     public void Collide(Collision2D c) {
-        //base.OnCollisionEnter2D(c);
+        base.OnCollisionEnter2D(c);
         if (c.gameObject.tag == "worm" && !c.gameObject.GetComponent<WormPart>().isHead && c.gameObject.GetComponent<WormPart>().state == WormPartStates.Detatched) {
             int insertIndex = bodyParts.IndexOf(bodyParts.Last(w => w.state == WormPartStates.Alive)) + 1;
             bodyParts.Remove(c.gameObject.GetComponent<WormPart>());
-            bodyParts.Insert(insertIndex, c.gameObject.GetComponent<WormPart>());
+            try {
+                bodyParts.Insert(insertIndex, c.gameObject.GetComponent<WormPart>());
+            }
+            catch (System.InvalidOperationException) {
+                insertIndex = 1;
+                bodyParts.Insert(insertIndex, c.gameObject.GetComponent<WormPart>());
+            }
             c.gameObject.GetComponent<WormPart>().Kill(WormPartStates.Alive);
             for (int i = 0; i < insertIndex; i++) {
                 bodyParts[i].transform.localPosition = partPositions[i];
+                bodyParts[i].transform.localRotation = Quaternion.identity;
+                bodyParts[i].GetComponent<HingeJoint2D>().anchor = partAnchor;
+                bodyParts[i].GetComponent<HingeJoint2D>().connectedAnchor = partConnectedAnchor;
                 if (i < bodyParts.Count - 1) {
                     bodyParts[i].GetComponent<HingeJoint2D>().connectedBody = bodyParts[i + 1].GetComponent<Rigidbody2D>();
                     if (bodyParts[i + 1].state == WormPartStates.Alive) {
@@ -55,6 +103,21 @@ public class Worm : Bug {
             }
         }
     }
+
+    public new void OnTriggerEnter2D(Collider2D c) {
+         if (c.tag == "berry" && !holdingBerry) {
+             moveSpeed = origMoveSpeed * berrySpeedMultiplier;
+             maxSpeed = origMaxSpeed * berrySpeedMultiplier;
+             berry = GameObject.Instantiate(c.gameObject, transform.position, Quaternion.identity) as GameObject;
+             berry.GetComponent<Collider2D>().enabled = false;
+             berry.transform.parent = head.transform;
+             berry.transform.localPosition = new Vector3(0, 0, 0);
+             holdingBerry = true;
+             c.gameObject.SetActive(false);
+             GetComponent<AudioSource>().clip = berrySnatchSound;
+             GetComponent<AudioSource>().Play();
+         }
+     }
 
     override protected void Move() {
         Vector2 velocity = head.GetComponent<Rigidbody2D>().velocity;
--- Assets/scripts/Bugs/Worm/WormPart.cs
+++ Assets/scripts/Bugs/Worm/WormPart.cs
@@ -27,49 +27,63 @@ public class WormPart : MonoBehaviour {
             StartCoroutine(KillRoutine());
     }
     public void Kill(WormPartStates killState) {
-        switch (killState) {
-            case WormPartStates.Alive:
-                state = WormPartStates.Alive;
-                break;
-            case WormPartStates.Detatched:
-                transform.parent.GetComponent<Worm>().Kill(this);
-                state = WormPartStates.Detatched;
-                break;
-            case WormPartStates.Dead:
-                GetComponent<SpriteRenderer>().sprite = dead;
-                GetComponent<Rigidbody2D>().isKinematic = true;
-                state = WormPartStates.Dead;
-                break;
-            case WormPartStates.Deader:
-                GetComponent<SpriteRenderer>().sprite = deader;
-                state = WormPartStates.Deader;
-                break;
+        if (!isHead && !transform.parent.GetComponent<Worm>().holdingBerry) {
+            switch (killState) {
+                case WormPartStates.Alive:
+                    state = WormPartStates.Alive;
+                    break;
+                case WormPartStates.Detatched:
+                    transform.parent.GetComponent<Worm>().Kill(this);
+                    state = WormPartStates.Detatched;
+                    break;
+                case WormPartStates.Dead:
+                    GetComponent<SpriteRenderer>().sprite = dead;
+                    GetComponent<Rigidbody2D>().isKinematic = true;
+                    state = WormPartStates.Dead;
+                    break;
+                case WormPartStates.Deader:
+                    GetComponent<SpriteRenderer>().sprite = deader;
+                    state = WormPartStates.Deader;
+                    break;
+            }
         }
     }
     IEnumerator KillRoutine() {
         killing = true;
-        switch (state) {
-            case WormPartStates.Alive:
-                transform.parent.GetComponent<Worm>().Kill(this);
-                yield return new WaitForSeconds(attatchDelay);
-                state = WormPartStates.Detatched;
-                break;
-            case WormPartStates.Detatched:
-                GetComponent<SpriteRenderer>().sprite = dead;
-                GetComponent<Rigidbody2D>().isKinematic = true;
-                state = WormPartStates.Dead;
-                break;
-            case WormPartStates.Dead:
-                GetComponent<SpriteRenderer>().sprite = deader;
-                state = WormPartStates.Deader;
-                break;
+        if (!isHead && !transform.parent.GetComponent<Worm>().holdingBerry) {
+            switch (state) {
+                case WormPartStates.Alive:
+                    transform.parent.GetComponent<Worm>().Kill(this);
+                    yield return new WaitForSeconds(attatchDelay);
+                    state = WormPartStates.Detatched;
+                    break;
+                case WormPartStates.Detatched:
+                    GetComponent<SpriteRenderer>().sprite = dead;
+                    GetComponent<Rigidbody2D>().isKinematic = true;
+                    state = WormPartStates.Dead;
+                    break;
+                case WormPartStates.Dead:
+                    GetComponent<SpriteRenderer>().sprite = deader;
+                    state = WormPartStates.Deader;
+                    break;
+            }
         }
-    }
-    void OnTriggerExit2D() {
+        else if(isHead){
+            transform.parent.GetComponent<Worm>().Kill(this);
+        }
+        yield return new WaitForSeconds(0.2f);
         killing = false;
     }
+    //void OnTriggerExit2D(Collider2D c) {
+    //    if(c.tag == "swatter")
+    //        killing = false;
+    //}
 
     void OnCollisionEnter2D(Collision2D c) {
         transform.parent.GetComponent<Worm>().Collide(c);
+    }
+
+    void OnTriggerEnter2D(Collider2D c) {
+        transform.parent.GetComponent<Worm>().OnTriggerEnter2D(c);
     }
 }

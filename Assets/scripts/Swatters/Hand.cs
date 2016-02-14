7bfdd3a269705a69836eaf2bb9461338c845a3fc
Tobi Hahn <rainshapes@gmail.com>
2016-02-14 11:58:01 -0500

    switch swatter movement to mouse delta

--- Assets/scripts/Swatters/Hand.cs
+++ Assets/scripts/Swatters/Hand.cs
@@ -17,10 +17,13 @@ public class Hand : MonoBehaviour {
 	Vector3 lastStuckPosition = Vector3.zero;
 	Vector3 lastAnimPosition = Vector3.zero;
 	Vector3 firstAnimPosition;
+    Vector3 lastMousePosition = Vector3.zero;
+    Vector3 mouseDelta = Vector3.zero;
 	protected virtual void Start(){
 		anim = transform.Find("anim").GetComponent<Animator>();
 		firstAnimPosition = anim.transform.localPosition;
         transform.Find("control").GetComponent<ControlDisplay>().Fade();
+        lastMousePosition = Input.mousePosition;
 	}
 	
 	// Update is called once per frame
@@ -45,16 +48,20 @@ public class Hand : MonoBehaviour {
 		}
 	}
     protected virtual void Update(){
+        mouseDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.ScreenToWorldPoint(lastMousePosition);
+        lastMousePosition = Input.mousePosition;
         lastAnimPosition = anim.transform.position;
         if(!GameObject.Find("GameManager").GetComponent<GameManager>().paused){
-            if (Input.GetJoystickNames().Length < 2) {
+            /*if (Input.GetJoystickNames().Length < 2) {
                 Vector3 mousePosition = Input.mousePosition;
                 mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
                 transform.position = Vector2.Lerp(transform.position, mousePosition, 1);
             }
             else {
                 transform.position += new Vector3(Input.GetAxis("Swatter X"), Input.GetAxis("Swatter Y"));
-            }
+            }*/
+            transform.position += mouseDelta;
+            transform.position += new Vector3(Input.GetAxis("Swatter X"), Input.GetAxis("Swatter Y"));
 		    if(Input.GetButtonDown("Swat") && !anim.GetCurrentAnimatorStateInfo(0).IsName("swat")){
 			    GetComponent<AudioSource>().clip = miss;
 			    GetComponent<AudioSource>().Play();
